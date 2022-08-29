4b866b33b850598b0d8771c77fa570eb671f0d4b
usefulsplash <emjavier16@gmail.com>
2022-08-29 14:23:36 -0400

    refactored addTodo function

--- function.js
+++ function.js
@@ -1,10 +1,35 @@
-let newTodo = document.querySelector("#newTodo");
-let addToDo = document.querySelector("#addTodo");
-let toDoList = document.querySelector("#toDoList");
+//Selectors
+const todoInput = document.querySelector("#todo-text");
+const todoButton = document.querySelector("#todo-button");
+const toDoList = document.querySelector("#todo-list");
 
-addToDo.onclick = () => {
-  let item = document.createElement("li");
-  item.textContent = newTodo.value;
-  toDoList.appendChild(item);
-  newTodo.value = null;
-};
+//Event Listeners
+todoButton.addEventListener("click", addTodo);
+
+//Functions
+function addTodo(e) {
+  //prevents form from submitting
+  e.preventDefault();
+  //creates div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  //creates list item
+  const newItem = document.createElement("li");
+  newItem.textContent = todoInput.value;
+  newItem.classList.add("todo-item");
+  todoDiv.append(newItem);
+  //creates done button
+  const doneButton = document.createElement("button");
+  doneButton.innerHTML = "done";
+  doneButton.classList.add("done-btn");
+  todoDiv.appendChild(doneButton);
+  //creates delete button
+  const deleteButton = document.createElement("Button");
+  deleteButton.innerHTML = "delete";
+  deleteButton.classList.add("delete-btn");
+  todoDiv.appendChild(deleteButton);
+  //appends whole div with children to todo list
+  toDoList.appendChild(todoDiv);
+  //clears input field
+  todoInput.value = null;
+}
